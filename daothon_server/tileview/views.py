0beb766f9ba49cb5556308c98d3b47fab5c345e7
Noctoid <i@noctoid.com>
2020-02-16 00:37:13 -0700

    filled all projects with content from matataki.io

--- daothon_server/tileview/views.py
+++ daothon_server/tileview/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+access_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJpQG5vY3RvaWQuY29tIiwiZXhwIjoxNTgyNDA4OTA4NzAwLCJwbGF0Zm9ybSI6ImVtYWlsIiwiaWQiOjE4NTB9.r9Cy3RKI_sX9Hy_m-ahZHGFhbXHV4CyxMEylIMCmL80"
+
+# Create your views here
 def all_projects(request):
+    raw_response = requests.get("https://api.smartsignature.io/token/all", headers={"x-access-token": access_token},
+                                params={"pagesize": 100, "sort": "general", "page": 1}).json().get("data").get("list")
     return render(
         request, "tileview.html", 
-        {"username": "Alice Volfield", "entries": [i for i in range(100)]}
+        {"username": "Alice Volfield", "entries": [i for i in raw_response]}
     )
 
 
