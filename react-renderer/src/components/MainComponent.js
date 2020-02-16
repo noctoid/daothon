1f282e349d2e177f573a13b1ebbac9347abd598f
CitruXonve <silveritdean@gmail.com>
2020-02-15 23:24:06 -0700

    Initialize chart components based on react

---
+++ react-renderer/src/components/MainComponent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return(
+            <div>a</div>
+        )
+    }
+}
+
+export default Main;
---
+++ react-renderer/src/components/PieChartComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PieChart extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            
+        )
+    }
+}
